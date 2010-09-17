fcc8dfae1b66d1e0c990d72973a0bd24fc19e47b
Shin Yee <shinyee@speedgocomputing.com>
2010-09-17 09:04:54 +0800

    License: Applying GPLv3.
    Signed-off-by: Shin Yee <shinyee@speedgocomputing.com>

---
+++ src/cuda/driver/rubycu.cpp
@@ -0,0 +1,22 @@
+/*
+# Copyright (c) 2010 Chung Shin Yee
+#
+#       shinyee@speedgocomputing.com
+#       http://www.speedgocomputing.com
+#       http://github.com/xman/sgc-ruby-cuda
+#
+# This file is part of sgc-ruby-cuda.
+#
+# sgc-ruby-cuda is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sgc-ruby-cuda is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with sgc-ruby-cuda.  If not, see <http://www.gnu.org/licenses/>.
+*/
