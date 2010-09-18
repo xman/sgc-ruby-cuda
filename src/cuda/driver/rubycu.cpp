8f710a4348f296c49fde2dffee1d3c0d224ec91c
Shin Yee <shinyee@speedgocomputing.com>
2010-09-18 14:45:30 +0800

    License: Update the package name to SGC-Ruby-CUDA.
    Signed-off-by: Shin Yee <shinyee@speedgocomputing.com>

--- src/cuda/driver/rubycu.cpp
+++ src/cuda/driver/rubycu.cpp
@@ -5,20 +5,20 @@
 #       http://www.speedgocomputing.com
 #       http://github.com/xman/sgc-ruby-cuda
 #
-# This file is part of sgc-ruby-cuda.
+# This file is part of SGC-Ruby-CUDA.
 #
-# sgc-ruby-cuda is free software: you can redistribute it and/or modify
+# SGC-Ruby-CUDA is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# sgc-ruby-cuda is distributed in the hope that it will be useful,
+# SGC-Ruby-CUDA is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with sgc-ruby-cuda.  If not, see <http://www.gnu.org/licenses/>.
+# along with SGC-Ruby-CUDA.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <cstring>
