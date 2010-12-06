ec1c58b72de7271f5409e41a5c5ec651b137ad38
Shin Yee <shinyee@speedgocomputing.com>
2010-12-06 10:38:11 +0800

    FFI: Extend FFI::Struct with methods to members.
    Signed-off-by: Shin Yee <shinyee@speedgocomputing.com>

---
+++ lib/ffi/prettystruct.rb
@@ -0,0 +1,28 @@
+require 'ffi'
+
+
+module FFI
+
+# This class is obtained from ffi-tk (https://github.com/Tass/ffi-tk).
+class PrettyStruct < FFI::Struct
+    ACCESSOR_CODE = <<-CODE
+        def {name}; self[{sym}]; end
+        def {name}=(value) self[{sym}] = value; end
+    CODE
+
+    def self.layout(*kvs)
+        kvs.each_slice(2) do |key, value|
+            eval ACCESSOR_CODE.gsub(/\{(.*?)\}/, '{name}' => key, '{sym}' => ":#{key}")
+        end
+
+        super
+    end
+
+    def inspect
+        kvs = members.zip(values)
+        kvs.map!{|key, value| "%s=%s" % [key, value.inspect] }
+        "<%s %s>" % [self.class, kvs.join(' ')]
+    end
+end
+
+end # module
