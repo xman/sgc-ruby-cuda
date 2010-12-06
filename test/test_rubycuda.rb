20b0b0cfdda01017dc9313a369091ce71909763b
Shin Yee <shinyee@speedgocomputing.com>
2010-12-06 12:20:08 +0800

    Test|CUDA: Initial test file for Ruby bindings of CUDA Runtime API.
    Signed-off-by: Shin Yee <shinyee@speedgocomputing.com>

---
+++ test/test_rubycuda.rb
@@ -0,0 +1,34 @@
+#-----------------------------------------------------------------------
+# Copyright (c) 2010 Chung Shin Yee
+#
+#       shinyee@speedgocomputing.com
+#       http://www.speedgocomputing.com
+#       http://github.com/xman/sgc-ruby-cuda
+#       http://rubyforge.org/projects/rubycuda
+#
+# This file is part of SGC-Ruby-CUDA.
+#
+# SGC-Ruby-CUDA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SGC-Ruby-CUDA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SGC-Ruby-CUDA.  If not, see <http://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------
+
+require 'test/unit'
+require 'rubycuda'
+
+include SGC::Cuda
+
+DEVID = ENV['DEVID'].to_i
+
+
+class TestRubyCuda < Test::Unit::TestCase
+end
