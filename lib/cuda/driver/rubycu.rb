4a048892e754c45de25f89b22f1e6b3aac443022
Shin Yee <shinyee@speedgocomputing.com>
2011-04-16 15:27:19 +0800

    CU: Header file.
    Signed-off-by: Shin Yee <shinyee@speedgocomputing.com>

---
+++ lib/cuda/driver/rubycu.rb
@@ -0,0 +1,37 @@
+#
+# Copyright (c) 2011 Chung Shin Yee
+#
+#       shinyee@speedgocomputing.com
+#       http://www.speedgocomputing.com
+#       http://github.com/xman/sgc-ruby-cuda
+#       http://rubyforge.org/projects/rubycuda
+#
+# This file is part of SGC-Ruby-CUDA.
+#
+# SGC-Ruby-CUDA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SGC-Ruby-CUDA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SGC-Ruby-CUDA.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'cuda/driver/ffi-cu'
+require 'cuda/driver/cu'
+require 'cuda/driver/error'
+require 'cuda/driver/init'
+require 'cuda/driver/version'
+require 'cuda/driver/device'
+require 'cuda/driver/context'
+require 'cuda/driver/deviceptr'
+require 'cuda/driver/function'
+require 'cuda/driver/memory'
+require 'cuda/driver/module'
+require 'cuda/driver/stream'
+require 'cuda/driver/event'
