86710e7a02400367b6dcfe796fa90c721f4205ae
Shin Yee <shinyee@speedgocomputing.com>
2011-04-17 10:55:54 +0800

    License: Update all files for year 2011.
    Signed-off-by: Shin Yee <shinyee@speedgocomputing.com>

--- test/test_rubycuda.rb
+++ test/test_rubycuda.rb
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------
-# Copyright (c) 2010 Chung Shin Yee
+# Copyright (c) 2011 Chung Shin Yee
 #
 #       shinyee@speedgocomputing.com
 #       http://www.speedgocomputing.com
