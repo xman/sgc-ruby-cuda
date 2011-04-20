dfd98a038120fedb7af04046e7e651f7599678a3
Shin Yee <shinyee@speedgocomputing.com>
2011-04-20 22:36:30 +0800

    CUDA: Update CUDA Device Management API for new Ruby bindings.
    Signed-off-by: Shin Yee <shinyee@speedgocomputing.com>

--- lib/cuda/runtime/device.rb
+++ lib/cuda/runtime/device.rb
@@ -31,67 +31,142 @@ module Cuda
 
 class CudaDevice
 
+    # @return [Integer] The number of CUDA devices.
     def self.count
         p = FFI::MemoryPointer.new(:int)
         status = API::cudaGetDeviceCount(p)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to get device count.")
         p.read_int
     end
 
 
+    # @return [Integer] The index of the current CUDA device in use.
     def self.get
         p = FFI::MemoryPointer.new(:int)
         status = API::cudaGetDevice(p)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to get current device.")
         p.read_int
     end
+    class << self; alias_method :current, :get; end
 
-    def self.current; self.get; end
 
-
+    # Set _devid_ as the current CUDA device.
+    # @param [Integer] devid The index (0..CudaDevice.count-1) of the CUDA device to set as current.
+    # @return [Class] This class.
     def self.set(devid)
         status = API::cudaSetDevice(devid)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to set current device: devid = #{devid}.")
         self
     end
     class << self; alias_method :current=, :set; end
 
 
+    # @param [CudaDeviceProp] prop The criteria for choosing a CUDA device.
+    # @return [Integer] The index of the CUDA device best matches the criteria.
     def self.choose(prop)
         pdev = FFI::MemoryPointer.new(:int)
         status = API::cudaChooseDevice(pdev, prop.to_ptr)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to choose a device with criteria.")
         pdev.read_int
     end
 
 
+    # @param [Integer] devid The index of the device to query.
+    # @return [CUDeviceProp] The properties of the device _devid_.
     def self.properties(devid = self.get)
         prop = CudaDeviceProp.new
         status = API::cudaGetDeviceProperties(prop.to_ptr, devid)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to get device properties: devid = #{devid}.")
         prop
     end
 
 
+    # Set the flags to be used for device execution.
+    # @param [Integer, CudaDeviceFlags, Array<Integer, CudaDeviceFlags>] flags The flags for device execution.
     def self.flags=(flags)
-        if flags.is_a?(Symbol)
-            flags = CudaDeviceFlags[flags]
-        end
-
-        status = API::cudaSetDeviceFlags(flags)
-        Pvt::handle_error(status)
-        flags
+        f = CudaDeviceFlags.value(flags)
+        status = API::cudaSetDeviceFlags(f)
+        Pvt::handle_error(status, "Failed to set device flags: flags = #{flags}.")
     end
 
 
+    # Set the list of CUDA devices that can be used.
+    # @param [Array] devs The list of CUDA device indexes.
     def self.valid_devices=(devs)
         p = FFI::MemoryPointer.new(:int, devs.count)
         devs.each_with_index do |devid, i|
             p[i].write_int(devid)
         end
         status = API::cudaSetValidDevices(p, devs.count)
-        Pvt::handle_error(status)
-        devs
+        Pvt::handle_error(status, "Failed to set valid devices: devs = #{devs}.")
+    end
+
+
+    # @return [CudaFunctionCache] The cache config of the current CUDA device.
+    #
+    # @since CUDA 4.0
+    def self.cache_config
+        p = FFI::MemoryPointer.new(:enum)
+        status = API::cudaDeviceGetCacheConfig(p)
+        Pvt::handle_error(status, "Failed to get the current CUDA device cache config.")
+        CudaFunctionCache[API::read_enum(p)]
+    end
+
+
+    # Set the cache config of the current CUDA device to _conf_.
+    # @param [CudaFunctionCache] conf The cache config of the current CUDA device to set to.
+    #
+    # @since CUDA 4.0
+    def self.cache_config=(conf)
+        status = API::cudaDeviceSetCacheConfig(conf)
+        Pvt::handle_error(status, "Failed to set the current CUDA device cache config.")
+    end
+
+
+    # @param [CudaLimit] lim The particular limit attribute to query.
+    # @return [CudaLimit] The limit _lim_ of the current CUDA device.
+    #
+    # @since CUDA 4.0
+    def self.limit(lim)
+        p = FFI::MemoryPointer.new(:size_t)
+        status = API::cudaDeviceGetLimit(p, lim)
+        Pvt::handle_error(status, "Failed to get the current CUDA device limit: limit = #{lim}.")
+        API::read_size_t(p)
+    end
+
+
+    # Set the limit _lim_ of the current CUDA device.
+    # @param [CudaLimit] lim The particular limit attribute to set.
+    # @param [Integer] value The value to set the limit to.
+    #
+    # @since CUDA 4.0
+    def self.limit=(*lim_val_pair)
+        lim, val = lim_val_pair.flatten
+        lim != nil && val != nil or raise ArgumentError, "Invalid limit and value pair given: limit = #{lim}, value = #{val}."
+        status = API::cudaDeviceSetLimit(lim, val)
+        Pvt::handle_error(status, "Failed to set the current CUDA device limit: limit = #{lim}, value = #{val}")
+    end
+
+
+    # Destroy all allocations and reset all state on the current CUDA device.
+    # @return [Class] This class.
+    #
+    # @since CUDA 4.0
+    def self.reset
+        status = API::cudaDeviceReset()
+        Pvt::handle_error(status, "Failed to reset the current CUDA device.")
+        self
+    end
+
+
+    # Block until all the tasks of the current CUDA device complete.
+    # @return [Class] This class.
+    #
+    # @since CUDA 4.0
+    def self.synchronize
+        status = API::cudaDeviceSynchronize()
+        Pvt::handle_error(status, "Failed to synchronize the current CUDA device.")
+        self
     end
 
 end
