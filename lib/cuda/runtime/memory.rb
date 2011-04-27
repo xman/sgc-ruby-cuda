519cc44e732ad85c835701cbe2bf1f1cdec73f93
Shin Yee <shinyee@speedgocomputing.com>
2011-04-27 11:22:12 +0800

    CUDA: Update CUDA Memory Management API.
    Signed-off-by: Shin Yee <shinyee@speedgocomputing.com>

--- lib/cuda/runtime/memory.rb
+++ lib/cuda/runtime/memory.rb
@@ -32,17 +32,30 @@ module Cuda
 
 class CudaDeviceMemory
 
+    # Allocate memory on the device.
+    # @param [Integer] nbytes The number of bytes of memory to allocate.
+    # @return [*SGC::Memory::MemoryPointer] A memory pointer to the allocated device memory. 
+    #
+    # @note The returned memory pointer is enabled to call _free_ method on itself.
     def self.malloc(nbytes)
         p = SGC::Memory::MemoryPointer.new
         status = API::cudaMalloc(p.ref, nbytes)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to allocate memory on the device: nbytes = #{nbytes}")
+        p.instance_eval %{
+            def free
+                CudaDeviceMemory.free(self)
+            end
+        }
         p
     end
 
 
+    # Free the device memory at _devptr_.
+    # @param [*SGC::Memory::MemoryPointer] devptr The memory pointer pointing to the device memory to be freed.
     def self.free(devptr)
         status = API::cudaFree(devptr.ptr)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to free the device memory.")
+        devptr.ptr = 0
         nil
     end
 
@@ -51,27 +64,41 @@ end
 
 module CudaMemory
 
+    # Copy _nbytes_ from the memory at _src_ptr_ to the memory at _dst_ptr_.
+    # @param [#ptr] dst_ptr Destination of the memory copy.
+    # @param [#ptr] src_ptr Source of the memory copy.
+    # @param [Integer] nbytes The number of bytes to copy.
+    # @param [Symbol] memcpy_kind The direction of the memory copy specified with one of the following:
+    #     * :HOST_TO_HOST
+    #     * :HOST_TO_DEVICE
+    #     * :DEVICE_TO_HOST
+    #     * :DEVICE_TO_DEVICE
     def memcpy(dst_ptr, src_ptr, nbytes, memcpy_kind)
         status = API::cudaMemcpy(dst_ptr.ptr, src_ptr.ptr, nbytes, memcpy_kind)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to copy memory.")
     end
     module_function :memcpy
 
+
+    # Copy _nbytes_ from the host memory at _src_ptr_ to the host memory at _dst_ptr_.
     def memcpy_htoh(dst_ptr, src_ptr, nbytes)
         memcpy(dst_ptr, src_ptr, nbytes, :HOST_TO_HOST)
     end
     module_function :memcpy_htoh
 
+    # Copy _nbytes_ from the host memory at _src_ptr_ to the device memory at _dst_ptr_.
     def memcpy_htod(dst_ptr, src_ptr, nbytes)
         memcpy(dst_ptr, src_ptr, nbytes, :HOST_TO_DEVICE)
     end
     module_function :memcpy_htod
 
+    # Copy _nbytes_ from the device memory at _src_ptr_ to the host memory at _dst_ptr_.
     def memcpy_dtoh(dst_ptr, src_ptr, nbytes)
         memcpy(dst_ptr, src_ptr, nbytes, :DEVICE_TO_HOST)
     end
     module_function :memcpy_dtoh
 
+    # Copy _nbytes_ from the device memory at _src_ptr_ to the device memory at _dst_ptr_.
     def memcpy_dtod(dst_ptr, src_ptr, nbytes)
         memcpy(dst_ptr, src_ptr, nbytes, :DEVICE_TO_DEVICE)
     end
