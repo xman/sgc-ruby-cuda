87daab6b775ab501c01bb1b9a9a7f5089860eb86
Shin Yee <shinyee@speedgocomputing.com>
2011-04-27 09:29:50 +0800

    CUDA: Update CUDA Execution Control API for new Ruby bindings.
    Signed-off-by: Shin Yee <shinyee@speedgocomputing.com>

--- lib/cuda/runtime/function.rb
+++ lib/cuda/runtime/function.rb
@@ -38,29 +38,36 @@ class CudaFunction
     attr_reader :name
 
 
+    # Create an instance to function _name_.
     def initialize(name)
         @name = name
     end
 
 
+    # @return [CudaFunctionAttributes] The attributes of this kernel function.
     def attributes
         a = CudaFunctionAttributes.new
         status = API::cudaFuncGetAttributes(a.to_ptr, @name)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to query function attributes.")
         a
     end
 
 
-    def cache_config=(config)
-        status = API::cudaFuncSetCacheConfig(@name, config)
-        Pvt::handle_error(status)
-        config
+    # Set the preferred cache configuration to use for next launch on this kernel function.
+    # @param [CudaFunctionCache] conf The preferred cache configuration.
+    def cache_config=(conf)
+        status = API::cudaFuncSetCacheConfig(@name, conf)
+        Pvt::handle_error(status, "Failed to set function cache config: config = #{conf}.")
     end
 
 
+    # Launch this kernel function with pre-configured settings.
+    # @return [Class] This class.
+    #
+    # @see .configure
     def launch
         status = API::cudaLaunch(@name)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to launch kernel function: name = #{@name}.")
         self
     end
 
@@ -75,49 +82,62 @@ class CudaFunction
     def self.configure(grid_dim, block_dim, shared_mem_size = 0, stream = 0)
         s = Pvt::parse_stream(stream)
         status = API::cudaConfigureCall(grid_dim, block_dim, shared_mem_size, s)
-        Pvt::handle_error(status)
+        Pvt::handle_error(status, "Failed to configure kernel function launch settings.\n" +
+            "* #{grid_dim.x} x #{grid_dim.y} x #{grid_dim.z} grid\n" +
+            "* #{block_dim.x} x #{block_dim.y} x #{block_dim.z} blocks\n" +
+            "* shared memory size = #{shared_mem_size}")
         self
     end
 
 
+    # Set the argument list of subsequent kernel function launch.
+    # @param [Array] *args The list of arguments to pass to the kernel.
+    # @return [Class] This class.
     def self.setup(*args)
         offset = 0
         args.each do |x|
             case x
                 when Fixnum
-                    p = FFI::MemoryPointer.new(:int)
-                    p.write_int(x)
+                    p = FFI::MemoryPointer.new(:int).write_int(x)
                     size = 4
                 when Float
-                    p = FFI::MemoryPointer.new(:float)
-                    p.write_float(x)
+                    p = FFI::MemoryPointer.new(:float).write_float(x)
                     size = 4
                 when SGC::Memory::MemoryPointer
                     p = x.ref
                     size = FFI::MemoryPointer.size
                 else
-                    raise TypeError, "Invalid type of argument #{x.to_s}."
+                    raise TypeError, "Invalid type of kernel parameters #{x}."
             end
             offset = align_up(offset, size)
             status = API::cudaSetupArgument(p, size, offset)
-            Pvt::handle_error(status)
+            Pvt::handle_error(status, "Failed to setup kernel argument for #{x}.")
             offset += size
         end
+        self
     end
 
 
+    # Load a dynamic library with _name_ from dynamic library path.
+    # @param [String] name The name of the dynamic library to load.
+    #     For library libcudart.so, its name is cudart.
+    # @return [Class] This class.
     def self.load_lib(name)
         raise NotImplementedError
     end
 
 
-    def self.load_lib_file(name)
-        @@libs << DL::dlopen(name)
-        # API::ffi_lib(name)
+    # Load a dynamic library from the given path.
+    # @param [String] path The path of the dynamic library to load.
+    # @return [Class] This class.
+    def self.load_lib_file(path)
+        @@libs << DL::dlopen(path)
         self
     end
 
 
+    # Unload all the loaded dynamic libraries.
+    # @return [Class] This class.
     def self.unload_all_libs
         @@libs.each do |h|
             h.close
